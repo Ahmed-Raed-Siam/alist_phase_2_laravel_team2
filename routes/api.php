17d8d553604b8795112d269442bedfcdf0abf073
Mohammed Salah Zourob <42889132+mohammedzourob@users.noreply.github.com>
2022-11-05 08:55:37 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -62,13 +62,9 @@ Route::post('{id}/delete-report', [ReportsController::class, 'destroy']);
 
 
 ///// Route Api Product ///////
-// Route::get('products', [ProdectController::class, 'index']);
-// Route::post('/products', [ProdectController::class, 'store']);
-// Route::get('products/{id}', [ProdectController::class, 'show']);
-// Route::post('products/{id}', [ProdectController::class, 'update']);
-// Route::delete('products/{id}',  [ProdectController::class, 'destroy']);
 
-Route::resource('products', ProdectController::class);
+
+// Route::resource('products', ProdectController::class);
 
 //   ******* Start Term & Conditions *******
 
@@ -106,6 +102,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('order-cases/{case}', [OrderCasesController::class, 'update']);
     Route::get('order-cases/{case}', [OrderCasesController::class, 'show']);
     Route::delete('order-cases/{case}', [OrderCasesController::class, 'destroy']);
+    
+    
+  Route::get('products', [ProdectController::class, 'index']);
+Route::post('/products', [ProdectController::class, 'store']);
+Route::get('products/{id}', [ProdectController::class, 'show']);
+Route::post('products/{id}', [ProdectController::class, 'update']);
+Route::delete('products/{id}',  [ProdectController::class, 'destroy']);
 
     Route::get('delivery-drivers', [DeliveryDriversController::class, 'index']);
     Route::post('delivery-drivers', [DeliveryDriversController::class, 'store']);
