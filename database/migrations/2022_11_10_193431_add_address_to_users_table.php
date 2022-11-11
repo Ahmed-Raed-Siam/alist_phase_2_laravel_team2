699d8f676263f66d52dd904e3d2521158944845c
thaerfayyad <eng.thaer94@gmail.com>
2022-11-11 19:04:10 +0200

    profile update

--- database/migrations/2022_11_10_193431_add_address_to_users_table.php
+++ database/migrations/2022_11_10_193431_add_address_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddProfileToUsersTable extends Migration
+class AddAddressToUsersTable extends Migration
 {
     /**
      * Run the migrations.
